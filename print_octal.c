f0dc4d39e17e7b84c645136eb3fc3d69d0219160
Aljawharah <aljawharhs18@gmail.com>
2025-11-19 19:22:23 +0300

    Refactor print functions: improve code formatting and add missing documentation comments

--- print_octal.c
+++ print_octal.c
@@ -1,24 +1,37 @@
 #include "main.h"
-
+/**
+* print_octal_rec - helper function to print an unsigned integer
+* in octal recursively.
+* @n: unsigned integer to print.
+* Return: number of characters printed.
+*/
 static int print_octal_rec(unsigned int n)
 {
 int count = 0;
+
 char c;
+
 if (n / 8)
 	count = count + print_octal_rec(n / 8);
 c = (n % 8) + '0';
-write (1 , &c, 1);
-return ( count + 1);
+write(1, &c, 1);
+return (count + 1);
 }
-
+/**
+* print_octal - function that prints an unsigned integer in octal.
+* @args: va_list containing the unsigned integer to print.
+* Return: number of characters printed.
+*/
 int print_octal(va_list args)
 {
-unsigned int n =va_arg(args,unsigned  int);
+unsigned int n = va_arg(args, unsigned  int);
+
 int count = 0;
+
 if (n == 0)
 {
 write(1, "0", 1);
-	return 1;
+	return (1);
 }
 count = print_octal_rec(n);
 return (count);
--- print_unsigned.c
+++ print_unsigned.c
@@ -1,27 +1,37 @@
 #include "main.h"
-
+/**
+ * print_int_rec - helper function to print an unsigned integer recursively.
+ * @n: unsigned integer to print.
+ * Return: number of characters printed.
+ */
 static int print_int_rec(unsigned int n)
 {
-int count = 0;
-char c;
-if (n / 10)
+	int count = 0;
+	char c;
+
+	if (n / 10)
 	{
-        count = count + print_int_rec(n / 10);
+		count = count + print_int_rec(n / 10);
 	}
 	c = (n % 10) + '0';
-        write (1, &c, 1);
-return (count + 1);
+	write(1, &c, 1);
+	return (count + 1);
 }
-
-int print_unsigned (va_list args)
+/**
+ * print_unsigned - function that prints an unsigned integer.
+ * @args: va_list containing the unsigned integer to print.
+ * Return: number of characters printed.
+ */
+int print_unsigned(va_list args)
 {
-unsigned int n = va_arg(args,unsigned int);
-int count = 0;
-if (n == 0)
+	unsigned int n = va_arg(args, unsigned int);
+	int count = 0;
+
+	if (n == 0)
 	{
-	write (1, "0", 1);
-	return 1;
+		write(1, "0", 1);
+		return (1);
 	}
-count = count + print_int_rec(n);
-return (count);
+	count = count + print_int_rec(n);
+	return (count);
 }
