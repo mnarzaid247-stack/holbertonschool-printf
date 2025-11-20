9f3ad2ed655eed8065f05032eb3e60f7e847a802
Aljawharah <aljawharhs18@gmail.com>
2025-11-20 14:35:10 +0300

    Refactor buffer_funcs.c for improved readability and consistency in formatting

--- buffer_funcs.c
+++ buffer_funcs.c
@@ -1,30 +1,30 @@
 #include "main.h"
+static char buffer[1024];
 
-char buffer[1024];
-int index = 0;
+static int index;
 
 int flush_buffer(void)
 {
-    if (index > 0)
-    {
-        write(1, buffer, index);
-        index = 0;
-    }
-    return (0);
+	if (index > 0)
+	{
+		write(1, buffer, index);
+		index = 0;
+	}
+	return (0);
 }
 
 int buffered_write(char c)
 {
-    if (index < 1024) 
-    {
-        buffer[index++] = c;
-        return (1);
-    }
-    else
-    {
-        flush_buffer();
-        buffer[index++] = c;
-        return (1);
-    }
-    return (0);
+	if (index < 1024)
+	{
+		buffer[index++] = c;
+		return (1);
+	}
+	else
+	{
+		flush_buffer();
+		buffer[index++] = c;
+		return (1);
+	}
+	return (0);
 }
