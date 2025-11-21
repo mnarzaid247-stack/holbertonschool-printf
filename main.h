c954385da7e3f6b1486bdc12c1d862d14514c64f
Aljawharah <aljawharhs18@gmail.com>
2025-11-21 17:27:51 +0300

    edited main.h to match the type of buffered functions

--- main.h
+++ main.h
@@ -27,7 +27,7 @@ int print_lower(va_list args);
 int print_upper(va_list args);
 int format_handler(int i, const char *format, va_list args,
 type_t table[]);
-int flush_buffer(void);
-int buffered_write(char c);
+void flush_buffer(void);
+void buffered_write(char c);
 
 #endif /* MAIN_H */
